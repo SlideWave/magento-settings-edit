93f5a750dd41f1feb2e502c5ca73888166d51498
David Daeschler <david.daeschler@gmail.com>
2016-07-28 10:59:55 -0500

    fix config parsing bug

--- edit-settings.php
+++ edit-settings.php
@@ -6,9 +6,9 @@
  *   to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *   
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *   
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -57,9 +57,9 @@ if (strpos($argv[2], ";") !== FALSE) {
 
 foreach ($changes as $change) {
     if (empty($change)) continue;
-    
+
     //Change will be in the form a.b.c.d='' or a.b.c.d=X where X is an integer
-    $pathAndValue = explode('=', $change);
+    $pathAndValue = explode('=', $change, 2);
     $value;
     if (startsWith($pathAndValue[1], "'")) {
         //string value
